a466499cbf7f730e4393dbd2337bc2427cb44f8e
Vlad <homyak72088@gmail.com>
2022-12-15 16:54:58 +0200

    Initial commit

---
+++ DZ_4_1.py
@@ -0,0 +1,8 @@
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+c = int(input('Enter third number: '))
+
+if a > 10 and b > 10 and c>10 and a % 3 == 0 and b % 3 == 0:
+    print ('yes')
+else:
+    print ('no')
