42c08a0285d207f1df4f77437c10313e4ad5667d
Vlad <homyak72088@gmail.com>
2022-12-19 19:05:22 +0200

    Fin

--- DZ_5_2.py
+++ DZ_5_2.py
@@ -1,12 +1,31 @@
 '''
 
+Задание со звездочкой:
+
 Вывести треугольник #3 с шириной N с помощью цикла while
+Вывести треугольник #4 с шириной N с помощью цикла while
 
 '''
 N = int(input('Enter the number: '))
 
-a = ' '
+b=0
+
 while N > 0 :
-    print(a, "*" * N)
+    print((b * " ") + ('*' * N))
+    b += 1
     N -= 1
+
+N = int(input('Enter the number: '))
+
+b=0
+
+while N >= 0:
+    if N == 0:
+        break
+    b += 1
+    N -= 1
+    print((N * " ") + ('*' * b))
+
+
+
 
