6236742118fb9bfd11248d9fb093ee17e39159b0
Vlad <homyak72088@gmail.com>
2022-12-27 13:11:58 +0200

    fin for dz 7

--- DZ_7_2.py
+++
@@ -1,9 +0,0 @@
-N = int(input('Enter number N: '))
-if N > 0:
-    a = input(f'Enter {N} numbers: ')
-    b = [a]
-    b = a.split()
-    print(sorted(b, reverse=True))
-else:
-    print('Wrong number')
-
