cdeea5c9b3a83c954415a9eec26600ac67403391
Vlad <homyak72088@gmail.com>
2023-01-30 19:38:18 +0200

    fin for dz 13

--- DZ_12.py
+++ DZ_12.py
@@ -18,13 +18,25 @@ n строк с именами файлов и допустимыми опера
 m запросов вида «операция файл».
 
 Для каждого допустимого запроса программа должна возвращать OK, для недопустимого – Access denied.
+3
+python.exe X
+book.txt R W
+notebook.exe R W X
+5
+read python.exe
+read book.txt
+write notebook.exe
+execute notebook.exe
+write book.txt
 """
 n = int(input('Enter the number of files: '))
 access_rights = {'R': 'read',
                  'X': 'execute',
                  'W': 'write'}
 files = {}
-
+answer = []
+x = 'OK'
+y = 'Access denied'
 
 for i in range(n):
     a = input('Enter your file: ').split()
@@ -33,6 +45,8 @@ m = int(input('Enter the number of file requests: '))
 for i in range(m):
     premissions, b = input('Enter validation and file: ').split()
     if premissions in files[b]:
-        print('OK')
+        answer.append(x)
     else:
-        print('Access denied')
+        answer.append(y)
+for i in range(len(answer)):
+     print(answer[i])
