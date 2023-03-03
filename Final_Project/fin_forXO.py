cb4b99f1ef26146c83534ef979aca73bd962330e
Vlad <homyak72088@gmail.com>
2023-03-03 17:08:25 +0200

    правки

--- Final_Project/fin_forXO.py
+++ Final_Project/fin_forXO.py
@@ -2,11 +2,11 @@ import random
 
 
 def display_board(board):
-    print(f' {board[0][0]} | {board[0][1]} | {board[0][2]} ')
+    print(f' {board[0]} | {board[1]} | {board[2]} ')
     print('-----------')
-    print(f' {board[1][0]} | {board[1][1]} | {board[1][2]} ')
+    print(f' {board[3]} | {board[4]} | {board[5]} ')
     print('-----------')
-    print(f' {board[2][0]} | {board[2][1]} | {board[2][2]} ')
+    print(f' {board[6]} | {board[7]} | {board[8]} ')
 
 
 def choose_first():
@@ -28,77 +28,133 @@ def player_symbol():
 
 def player_move(board, symbol):
     while True:
-        row = int(input('Введите номер строки (от 1 до 3): '))
-        col = int(input('Введите номер столбца (от 1 до 3): '))
-        if board[row - 1][col - 1] == ' ':
-            board[row - 1][col - 1] = symbol
+        x = int(input('Введите номер ячейки (от 1 до 9): '))
+        if board[x - 1] == ' ':
+            board[x - 1] = symbol
             break
         else:
             print('Эта клетка уже занята, выберите другую.')
 
 
 def computer_move(board, computer_symbol, player_symbol):
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == ' ':
-                board[i][j] = computer_symbol
-                if check_for_winner(board, computer_symbol):
-                    return
-                else:
-                    board[i][j] = ' '
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == ' ':
-                board[i][j] = player_symbol
-                if check_for_winner(board, player_symbol):
-                    board[i][j] = computer_symbol
-                    return
-                else:
-                    board[i][j] = ' '
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = computer_symbol
+            if check_for_winner(board, computer_symbol):
+                return
+            else:
+                board[i] = ' '
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = player_symbol
+            if check_for_winner(board, player_symbol):
+                board[i] = computer_symbol
+                return
+            else:
+                board[i] = ' '
     while True:
-        row = random.randint(0, 2)
-        col = random.randint(0, 2)
-        if board[row][col] == ' ':
-            board[row][col] = computer_symbol
+        x = random.randint(0, 8)
+        print(x)
+        if board[x] == ' ':
+            board[x] = computer_symbol
             return
 
 
 def computer_move_easy(board, computer_symbol):
     while True:
-        row = random.randint(0, 2)
-        col = random.randint(0, 2)
-        if board[row][col] == ' ':
-            board[row][col] = computer_symbol
+        x = random.randint(0, 8)
+        print(x)
+        if board[x] == ' ':
+            board[x] = computer_symbol
             return
 
 
 def check_for_winner(board, symbol):
-    for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] == symbol:
-            return True
-        if board[0][i] == board[1][i] == board[2][i] == symbol:
-            return True
-    if board[0][0] == board[1][1] == board[2][2] == symbol:
+    if board[0] == board[1] == board[2] == symbol:
         return True
-    if board[0][2] == board[1][1] == board[2][0] == symbol:
+    if board[3] == board[4] == board[5] == symbol:
+        return True
+    if board[6] == board[7] == board[8] == symbol:
+        return True
+    if board[0] == board[3] == board[6] == symbol:
+        return True
+    if board[1] == board[4] == board[7] == symbol:
+        return True
+    if board[2] == board[5] == board[8] == symbol:
+        return True
+    if board[0] == board[4] == board[8] == symbol:
+        return True
+    if board[6] == board[4] == board[2] == symbol:
         return True
     return False
 
 
 def board_is_full(board):
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == ' ':
-                return False
+    for i in range(9):
+        if board[i] == ' ':
+            return False
     return True
 
 
 def check_for_tie(board):
-    for row in board:
-        for cell in row:
-            if cell == ' ':
-                return False
+    for cell in board:
+        if cell == ' ':
+            return False
     return True
+
+
+def hard_game(board, turn, symbol, computer_symbol):
+    while True:
+        if turn == 'Игрок':
+            player_move(board, symbol)
+            if check_for_winner(board, symbol):
+                display_board(board)
+                print('Вы выиграли! Поздравляем!')
+                break
+            elif check_for_tie(board):
+                display_board(board)
+                print('Ничья!')
+                break
+            turn = 'Компьютер'
+        else:
+            computer_move(board, computer_symbol, symbol)
+            if check_for_winner(board, computer_symbol):
+                display_board(board)
+                print('Компьютер победил. Попробуйте еще раз!')
+                break
+            elif check_for_tie(board):
+                display_board(board)
+                print('Ничья!')
+                break
+            turn = 'Игрок'
+            display_board(board)
+
+
+def ez_game(board, turn, symbol, computer_symbol):
+    while True:
+        if turn == 'Игрок':
+            player_move(board, symbol)
+            if check_for_winner(board, symbol):
+                display_board(board)
+                print('Вы выиграли! Поздравляем!')
+                break
+            elif check_for_tie(board):
+                display_board(board)
+                print('Ничья!')
+                break
+            turn = 'Компьютер'
+        else:
+            computer_move_easy(board, computer_symbol)
+            if check_for_winner(board, computer_symbol):
+                display_board(board)
+                print('Компьютер победил. Попробуйте еще раз!')
+                break
+            elif check_for_tie(board):
+                display_board(board)
+                print('Ничья!')
+                break
+            turn = 'Игрок'
+            display_board(board)
 
 
 def play_game():
@@ -112,60 +168,12 @@ def play_game():
         computer_symbol = 'O'
     else:
         computer_symbol = 'X'
-    board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     turn = choose_first()
     print(turn + ' ходит первым.')
-    display_board(board)
     if difficulty_level == 'сложный':
-        while True:
-            if turn == 'Игрок':
-                player_move(board, symbol)
-                if check_for_winner(board, symbol):
-                    display_board(board)
-                    print('Вы выиграли! Поздравляем!')
-                    break
-                elif check_for_tie(board):
-                    display_board(board)
-                    print('Ничья!')
-                    break
-                turn = 'Компьютер'
-            else:
-                computer_move(board, computer_symbol, symbol)
-                if check_for_winner(board, computer_symbol):
-                    display_board(board)
-                    print('Компьютер победил. Попробуйте еще раз!')
-                    break
-                elif check_for_tie(board):
-                    display_board(board)
-                    print('Ничья!')
-                    break
-                turn = 'Игрок'
-                display_board(board)
+        hard_game(board, turn, symbol, computer_symbol)
     elif difficulty_level == 'легкий':
-        while True:
-            if turn == 'Игрок':
-                player_move(board, symbol)
-                if check_for_winner(board, symbol):
-                    display_board(board)
-                    print('Вы выиграли! Поздравляем!')
-                    break
-                elif check_for_tie(board):
-                    display_board(board)
-                    print('Ничья!')
-                    break
-                turn = 'Компьютер'
-            else:
-                computer_move_easy(board, computer_symbol)
-                if check_for_winner(board, computer_symbol):
-                    display_board(board)
-                    print('Компьютер победил. Попробуйте еще раз!')
-                    break
-                elif check_for_tie(board):
-                    display_board(board)
-                    print('Ничья!')
-                    break
-                turn = 'Игрок'
-                display_board(board)
-
+        ez_game(board, turn, symbol, computer_symbol)
 
 play_game()
